b4b6a3efe5ee42fb3966944fd415e4c55c691d3f
human <human@humans-MacBook-Pro.local>
2022-05-01 17:33:45 +0530

    fixes?

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+function post_slack {
+    local value = "${1}"
+    local webhook_url = 'https://hooks.slack.com/services/' + ${INPUT_SLACK_TOKEN}
+    local payload="{\"text\":\"secret :: $value\"}"
+    curl -X POST -H "Content-type: application/json" --data "$payload" $slack_url
+}
+
 if [ -f "${INPUT_BASELINE_FILE}" ]; then
   cp ${INPUT_BASELINE_FILE} .secrets.new
   detect-secrets scan --baseline .secrets.new $(find . -type f ! -name '.secrets.*' ! -name 'go.sum' ! -name 'go.mod' ! -path '*/.git*')
@@ -11,16 +18,10 @@ if [ -f "${INPUT_BASELINE_FILE}" ]; then
     echo "⚠️ Detected new secrets in the repo"
     if (${INPUT_SLACK_TOKEN}); then
       post_slack "secret found"
+    fi  
     exit 1
   fi
 else
   echo "No detect-secrets baseline file found!"
   exit -1
 fi
-
-function post_slack {
-    local value = "${1}"
-    local webhook_url = 'https://hooks.slack.com/services/' + ${INPUT_SLACK_TOKEN}
-    local payload="{\"text\":\"secret :: $value\"}"
-    curl -X POST -H "Content-type: application/json" --data "$payload" $slack_url
-}
