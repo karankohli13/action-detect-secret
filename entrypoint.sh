3bbce592ec3baed3bd2693b925cfb1542604d208
human <human@humans-MacBook-Pro.local>
2022-05-01 17:59:27 +0530

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 set -e
 
 function post_slack {
-    local value = "${1}"
-    local webhook_url = 'https://hooks.slack.com/services/' + ${INPUT_SLACK_TOKEN}
+    local value="$1"
+    echo $value
+    local webhook_url='https://hooks.slack.com/services/'${INPUT_SLACK_TOKEN}
     local payload="{\"text\":\"secret :: $value\"}"
-    curl -X POST -H "Content-type: application/json" --data "$payload" $slack_url
+    curl -X POST -H "Content-type: application/json" --data "$payload" $webhook_url
 }
 
-post_slack "secret found"
+post_slack "secret"
 
 if [ -f "${INPUT_BASELINE_FILE}" ]; then
   cp ${INPUT_BASELINE_FILE} .secrets.new
@@ -19,7 +20,7 @@ if [ -f "${INPUT_BASELINE_FILE}" ]; then
     echo ""
     echo "⚠️ Detected new secrets in the repo"
     if (${INPUT_SLACK_TOKEN}); then
-      post_slack "secret found"
+      post_slack "secret2"
     fi  
     exit 1
   fi
